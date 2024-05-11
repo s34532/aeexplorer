edf02746978552f98ab918fba3554ea9a565116b
jkh130 <mrchimega@gmail.com>
2024-05-10 21:24:57 -0500

    rebrand

--- src/renderer/Pages/HomePage/HomePage.tsx
+++ src/renderer/Pages/HomePage/HomePage.tsx
@@ -9,18 +9,47 @@ interface Props {}
 const HomePage = (props: Props) => {
   const [projectsLength, setProjectsLength] = useState(0);
   const [hasProjects, setHasProjects] = useState(false);
+  function sillyText(word) {
+    var span = [];
+    for (var i = 0; i < word.length; i++) {
+      span.push(
+        <motion.span
+          whileHover={{
+            fontWeight: 700,
+            transition: { duration: 0.05 },
+          }}
+          className="font-change"
+        >
+          {word[i]}
+        </motion.span>,
+      );
+    }
+    return span;
+  }
 
   function projectNames() {
     if (projectsLength >= 2) {
       return (
         <>
           <div className="big-title">
-            You have <span className="highlighted-text">{projectsLength}</span>{' '}
-            Projects!
+            <motion.div
+              whileTap={{ scale: 1.05 }}
+              whileHover={{ scale: [null, 1.035, 1.03] }}
+              drag
+              dragSnapToOrigin={true}
+              dragTransition={{
+                bounceStiffness: 500,
+                bounceDamping: 10,
+              }}
+              dragConstraints={{ left: 0, right: 0, top: 0, bottom: 0 }}
+              dragElastic={0.05}
+              transition={{ duration: 0.5, type: 'spring' }}
+            >
+              {sillyText('You Have')}{' '}
+              <span className="highlighted-text">{projectsLength}</span>{' '}
+              {sillyText('Projects!')}
+            </motion.div>
           </div>
-          <p className="homepage-text">
-            Continue optimizing your workflow today!
-          </p>
 
           <div className="homepage-button-container">
             <motion.div
@@ -44,20 +73,38 @@ const HomePage = (props: Props) => {
       return (
         <>
           <div className="big-title">
-            You have <span className="highlighted-text">{projectsLength}</span>{' '}
-            project waiting for you!
+            <motion.div
+              whileTap={{ scale: 1.05 }}
+              whileHover={{ scale: [null, 1.035, 1.03] }}
+              drag
+              dragSnapToOrigin={true}
+              dragTransition={{
+                bounceStiffness: 500,
+                bounceDamping: 10,
+              }}
+              dragConstraints={{ left: 0, right: 0, top: 0, bottom: 0 }}
+              dragElastic={0.05}
+              transition={{ duration: 0.5, type: 'spring' }}
+            >
+              <span className="highlighted-text">{projectsLength}</span>{' '}
+              {sillyText('Project!')}
+            </motion.div>
           </div>
-          <p className="homepage-text">
-            Start optimizing your workflow and create today!
-          </p>
 
           <div className="homepage-button-container">
-            <Link
-              to={hasProjects ? '/projects' : '/CreateAEP'}
-              className="start-button"
+            <motion.div
+              whileTap={{ scale: 0.9 }}
+              className="box"
+              whileHover={{ scale: 1.05 }}
+              transition={{ type: 'spring', stiffness: 400, damping: 10 }}
             >
-              <span className="button-text">View project</span>
-            </Link>
+              <Link
+                to={hasProjects ? '/projects' : '/CreateAEP'}
+                className="start-button"
+              >
+                <span className="button-text">View project</span>
+              </Link>
+            </motion.div>
           </div>
         </>
       );
@@ -96,21 +143,43 @@ const HomePage = (props: Props) => {
             ) : (
               <>
                 <div className="big-title">
-                  After Effects Project management made{' '}
-                  <span className="highlighted-text">effortless.</span>
+                  {
+                    <motion.div
+                      whileTap={{ scale: 1.05 }}
+                      whileHover={{ scale: [null, 1.035, 1.03] }}
+                      drag
+                      dragSnapToOrigin={true}
+                      dragTransition={{
+                        bounceStiffness: 500,
+                        bounceDamping: 10,
+                      }}
+                      dragConstraints={{ left: 0, right: 0, top: 0, bottom: 0 }}
+                      dragElastic={0.05}
+                      transition={{ duration: 0.5, type: 'spring' }}
+                    >
+                      {sillyText('Lets start.')}
+                    </motion.div>
+                  }
                 </div>
                 <p className="homepage-text">
-                  Organize your After Effects Projects all in one location.
+                  Organize your After Effects Projects All in One Location.
                 </p>
 
-                <div className="homepage-button-container">
-                  <Link
-                    to={hasProjects ? '/projects' : '/CreateAEP'}
-                    className="start-button"
-                  >
-                    <span className="button-text">Start now</span>
-                  </Link>
-                </div>
+                <motion.div
+                  whileTap={{ scale: 0.9 }}
+                  className="box"
+                  whileHover={{ scale: 1.05 }}
+                  transition={{ type: 'spring', stiffness: 400, damping: 10 }}
+                >
+                  <div className="homepage-button-container">
+                    <Link
+                      to={hasProjects ? '/projects' : '/CreateAEP'}
+                      className="start-button"
+                    >
+                      <span className="button-text">Start now</span>
+                    </Link>
+                  </div>
+                </motion.div>
               </>
             )}
           </div>
